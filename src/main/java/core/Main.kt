67e0a8256448df811862772f6ace9cccc8a5c2ec
nizienko <nizienko@outlook.com>
2018-01-20 15:05:57 +0300

    test branch

--- src/main/java/core/Main.kt
+++ src/main/java/core/Main.kt
@@ -3,7 +3,7 @@ package core
 import core.ctx.AppContext
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.TelegramBotsApi
-
+//test
 fun main(args: Array<String>) {
     ApiContextInitializer.init()
     val telegramApi = TelegramBotsApi()
